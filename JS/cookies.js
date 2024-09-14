17a366d41cd356f947d011bb67a697eb7b358444
itsisan <haghi.isan79@gmail.com>
2024-09-14 16:44:20 +0330

    version 1.3.5

---
+++ JS/cookies.js
@@ -0,0 +1,11 @@
+const cookies = document.querySelector("#cookies__wrapper");
+const close_cookies = document.querySelector("#close__cookie");
+const accept_cookies = document.querySelector("#accept__cookie");
+
+close_cookies.addEventListener("click", function () {
+  cookies.style.display = "none";
+});
+
+accept_cookies.addEventListener("click", function () {
+  cookies.style.display = "none";
+});
